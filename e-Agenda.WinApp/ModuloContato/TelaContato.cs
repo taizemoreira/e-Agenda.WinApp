2fd03df3f9e3e1d11b7fa40e6f90020bf9a43227
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-05-16 16:53:14 -0300

    Adicionar arquivos de projeto.

---
+++ e-Agenda.WinApp/ModuloContato/TelaContato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Agenda.WinApp.ModuloContato
+{
+    public partial class TelaContato : Form
+    {
+        public TelaContato()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ e-Agenda.WinApp/TelaPrincipal.cs
@@ -0,0 +1,10 @@
+namespace e_Agenda.WinApp
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
